cd993f35b4a3a72316c4c7876474a90ecab7f183
ketongwang <kentwang16@gmail.com>
2014-08-29 17:35:16 -0500

    If to be followed by whitespace

--- perlocation/Percolation.java
+++ perlocation/Percolation.java
@@ -21,7 +21,7 @@ public class Percolation {
      * @param N, dimension of the system
      */
     public Percolation(int N) {
-        if(N <= 0) throw new IllegalArgumentException();
+        if (N <= 0) throw new IllegalArgumentException();
         states = new int[N * N + 2];                 // SYNTAX, avoid NULL
         dim = N;
         cells = new WeightedQuickUnionUF(N * N + 2); // SYNTAX, additional top and bottom cells
@@ -42,8 +42,8 @@ public class Percolation {
         states[siteIndex] = 1;                       // open site (i, j)
         
         /** union to top/bottom if first/last row **/
-        if(i == 1) cells.union(siteIndex, dim * dim);
-        if(i == dim) cells.union(siteIndex, dim * dim  + 1);
+        if (i == 1) cells.union(siteIndex, dim * dim);
+        if (i == dim) cells.union(siteIndex, dim * dim  + 1);
         
         /** union neighbor cells **/
         if (inRangeBoolean(i - 1, j) && isOpen(i - 1, j)) 
