d5c202a192991d812e08db7b6dd6902c56cac01c
ketongwang <kentwang16@gmail.com>
2014-08-28 22:49:13 -0500

    Public interface intialization

---
+++ perlocation/PercolationStats.java
@@ -0,0 +1,48 @@
+/******************************************************************************
+  *  Compilation: javac-alg4 PercolationStats.java
+  *  Execution: java PercolationStats
+  * 
+  *  Percolation Monte Carlo experiment
+  *  Author: Ketong Wang
+  */
+
+public class PercolationStats {
+    // Some private names to hold data
+    private double[] thresholds;
+    
+    /**
+     * Initializes the percolation
+     */
+    public PercolationStats (int N, int T) { //N dimension, T repeating times
+        thresholds = double[]
+    }
+    
+    /**
+     * Sample mean of percolation threshold
+     */
+    public double mean() {}
+    
+    /**
+     * Sample standard deviation of percolation threshold
+     */
+    public double stddev() {}
+    
+    /**
+     * returns lower bound of the 95% confidence interval
+     */
+    public double confidenceLo() {}
+    
+    /**
+     * returns upper bound of the 95% confidence interval
+     */
+    public double confidenceHi() {}
+    
+    /**
+     * test client, described below
+     */
+    public static void main(String[] args) {}
+    
+    
+    
+    
+}
