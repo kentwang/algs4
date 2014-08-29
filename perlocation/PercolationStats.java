1ac02a128571e15430b1a51d81fa895b3773e163
ketongwang <kentwang16@gmail.com>
2014-08-29 13:57:49 -0500

    Initialize new variable

--- perlocation/PercolationStats.java
+++ perlocation/PercolationStats.java
@@ -14,33 +14,48 @@ public class PercolationStats {
      * Initializes the percolation
      */
     public PercolationStats (int N, int T) { //N dimension, T repeating times
-        thresholds = double[]
+        thresholds = new double[T];
+        for (int i = 0; i < T; i++) { // run T experiments
+            Percolation percolationItem = new Percolation(N);
+            StdOut.println(percolationItem.percolates());
+            while (!percolationItem.percolates()) {
+                int row = new StdRand.uniform(1, N + 1);
+                int col = new StdRand.uniform(1, N + 1);
+            }
+        }
     }
     
     /**
      * Sample mean of percolation threshold
      */
-    public double mean() {}
+//    public double mean() {}
     
     /**
      * Sample standard deviation of percolation threshold
      */
-    public double stddev() {}
+//    public double stddev() {}
     
     /**
      * returns lower bound of the 95% confidence interval
      */
-    public double confidenceLo() {}
+//    public double confidenceLo() {}
     
     /**
      * returns upper bound of the 95% confidence interval
      */
-    public double confidenceHi() {}
+//    public double confidenceHi() {}
     
     /**
      * test client, described below
      */
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        StdOut.print("Enter the grid dimension N: ");
+        int N = StdIn.readInt();
+        StdOut.print("Enter the repeating times of experiments T: ");
+        int T = StdIn.readInt();
+        
+        PercolationStats pStats = new PercolationStats(3, 100);
+    }
     
     
     
